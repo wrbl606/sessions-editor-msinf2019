813c15b0f8cec5fc98eb8c2fc083a27a4361901f
Maciej Skrabski <maciej.skrabski@gmail.com>
2019-03-31 22:29:00 +0200

    Added -l and -f verification.

--- app.ts
+++ app.ts
@@ -17,3 +17,34 @@ if (isInfo) {
   Number of gyroscope entries: ${gyroData.length}
   `);
 }
+if (accData.length < limit)
+  console.log(`Number of Accelerometer's records has to be greater than the record limit or equal. 
+Current limit: ${limit}. 
+The number of records in the file: ${accData.length}.
+You can inspect the number of records in the file by adding parameter -i or --info after the session name.
+You can change the record limit by adding parameter -l {number} or --limit {number} after the session name, replacing {number} with a number you want the limit to be. 
+  `);
+
+if (gyroData.length < limit)
+  console.log(`Number of Gyroscope's records has to be greater than the record limit or equal. 
+Current limit: ${limit}. 
+The number of records in the file: ${gyroData.length}.
+You can inspect the number of records in the file by adding parameter -i or --info after the session name.
+You can change the record limit by adding parameter -l {number} or --limit {number} after the session name, replacing {number} with a number you want the limit to be. 
+  `);
+
+if (accData.length < files)
+  console.log(`Number of Accelerometer's records has to be greater than the maximum number of output files or equal. 
+Current maximum number of output files: ${files}. 
+The number of records in the file: ${accData.length}.
+You can inspect the number of records in the file by adding parameter -i or --info after the session name.
+You can change the maximum number of output files by adding parameter -f {number} or --files {number} after the session name, replacing {number} with a number you want the limit to be. 
+  `);
+
+if (gyroData.length < files)
+  console.log(`Number of Gyroscope's records has to be greater than the maximum number of output files or equal. 
+Current maximum number of output files: ${files}. 
+The number of records in the file: ${gyroData.length}.
+You can inspect the number of records in the file by adding parameter -i or --info after the session name.
+You can change the maximum number of output files by adding parameter -f {number} or --files {number} after the session name, replacing {number} with a number you want the limit to be. 
+  `);
