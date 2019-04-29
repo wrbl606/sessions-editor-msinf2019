fab900ded0bfccd6c6813af2ab5bee31f90efcbb
Marcin Wróblewski <marcinwroblewski@outlook.com>
2019-04-29 14:14:44 +0200

    calucalting norm for each x,y,z value

--- src/SessionDataRow.ts
+++ src/SessionDataRow.ts
@@ -3,10 +3,12 @@ export default class SessionDataRow {
   x: number;
   y: number;
   z: number;
+  norm: number;
   constructor(time: number, x: number, y: number, z: number) {
     this.time = time;
     this.x = x;
     this.y = y;
     this.z = z;
+    this.norm = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
   }
 }
