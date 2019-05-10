dc01b1aae3c025019c3d7dc6228b5fd6994315a2
Maciej <maciej.skrabski@gmail.com>
2019-05-10 07:02:14 +0200

    Finding peaks and the time they occur at (needs refactoring and making it objective)

--- app.ts
+++ app.ts
@@ -11,7 +11,6 @@ if (!unzip.validateInputFiles())
 
 const accData: Array<SessionDataRow> = unzip.readAccelerometerDataToArray();
 const gyroData: Array<SessionDataRow> = unzip.readGyroDataToArray();
-
 let numOfFiles = parameters.files;
 let maxF;
 
@@ -31,15 +30,17 @@ if (maxF < numOfFiles) {
 if (numOfFiles == -1) numOfFiles = maxF;
 
 if (parameters.isInfo) {
-  const info = [{
-    sensor: 'Accelerometer',
-    time: accData[accData.length - 1].time - accData[0].time,
-    entries: accData.length
-  }, {
-    sensor: 'Gyroscope',
-    time: gyroData[gyroData.length - 1].time - gyroData[0].time,
-    entries: gyroData.length
-  },
+  const info = [
+    {
+      sensor: 'Accelerometer',
+      time: accData[accData.length - 1].time - accData[0].time,
+      entries: accData.length,
+    },
+    {
+      sensor: 'Gyroscope',
+      time: gyroData[gyroData.length - 1].time - gyroData[0].time,
+      entries: gyroData.length,
+    },
   ];
 
   console.info(`Session ${args[0]} details: `);
@@ -50,5 +51,47 @@ if (parameters.isInfo) {
 parameters.validateParameters(accData, gyroData, numOfFiles);
 
 save.createFolder(unzip.fileName);
-save.saveToCsv(accData, numOfFiles, unzip.fileName, parameters.limit, 'accelerometer');
+save.saveToCsv(
+  accData,
+  numOfFiles,
+  unzip.fileName,
+  parameters.limit,
+  'accelerometer'
+);
 save.saveToCsv(gyroData, numOfFiles, unzip.fileName, parameters.limit, 'gyro');
+
+var accValue = [];
+var accTime = [];
+
+for (let i = 1; i < accData.length - 1; i++)
+  if (
+    (accData[i].norm > accData[i - 1].norm &&
+      accData[i].norm > accData[i + 1].norm) ||
+    (accData[i].norm < accData[i - 1].norm &&
+      accData[i].norm < accData[i + 1].norm)
+  ) {
+    accValue.push(accData[i].norm);
+    accTime.push(accData[i].time);
+  }
+
+console.log('\n acc Peaks: \n');
+for (let i = 0; i <= accValue.length - 1; i++)
+  console.log(accValue[i], accTime[i]);
+
+var gyroValue = [];
+var gyroTime = [];
+
+for (let i = 1; i < gyroData.length - 1; i++)
+  if (
+    (gyroData[i].norm > gyroData[i - 1].norm &&
+      gyroData[i].norm > gyroData[i + 1].norm) ||
+    (gyroData[i].norm < gyroData[i - 1].norm &&
+      gyroData[i].norm < gyroData[i + 1].norm)
+  ) {
+    gyroValue.push(gyroData[i].norm);
+    gyroTime.push(gyroData[i].time);
+  }
+
+console.log('\n gyro Peaks: \n');
+for (let i = 0; i <= gyroValue.length - 1; i++)
+  console.log(gyroValue[i], gyroTime[i]);
